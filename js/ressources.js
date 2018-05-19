8533273a75ea16611b2646d6cb923bb8331389d2
bezounours <gratien.therond@gmail.com>
2018-05-19 22:03:34 +0200

    ajout clé image dans le json ressources

--- js/ressources.js
+++ js/ressources.js
@@ -2,19 +2,19 @@
 // pour les objert json l'accès aux valeurs (ex:"insulte") se fait à l'aide de clef (ex: auteur ou son ou titre) ce qui donne pour un tableau : tableau[0] pour la première valeur et objet .auteur pour la première valeur du json
 
 var ressources = [
-    {auteur:"Lopez Gitan", type:"voix", son:'gueule.mp3', titre:"Photo 1"},
-    {auteur:"kaaris", type:"voix",son:'kaaris.mp3', titre:"Photo 2"},
-    {auteur:"Antoine Daniel", type:"voix", son:'popopo.mp3', titre:"Photo 3"},
-    {auteur:"Antoine Daniel", type:"voix", son:'eddy.mp3', titre:"Photo 4"},
-    {auteur:"Antoine Daniel", type:"voix", son:'russie.mp3', titre:"Photo 5"},
-    {auteur:"Antoine Daniel", type:"voix", son:'psycho.mp3', titre:"Photo 6"},
-    {auteur:"Antoine Daniel", type:"voix", son:'salutAD.mp3', titre:"Photo 7"},
-    {auteur:"South Park", type:"animé", son:'gogole.mp3', titre:"Photo 8"},
-    {auteur:"leeroy Jenkins", type:"jeux", son:'leeroy.mp3', titre:"Photo 9"},
-    {auteur:"Wilhelm", type:"voix", son:'Wilhelm.mp3', titre:"Photo 10"},
-    {auteur:"Naheulbeuk", type:"voix", son:'nain.mp3', titre:"Photo 11"},
-    {auteur:"Game Of Thrones", type:"voix", son:'hodor.mp3', titre:"Photo 12"},
-    {auteur:"Kaamlott", type:"voix", son:'perceval.mp3', titre:"Photo 13"},
-    {auteur:"Kaamlott", type:"voix", son:'gras.mp3', titre:"Photo 14"}
-     ] 
+    { auteur: "Lopez Gitan", type: "voix", son: 'gueule.mp3', titre: "Photo 1", img: "img1.jpg"},
+    { auteur: "kaaris", type: "voix", son: 'kaaris.mp3', titre: "Photo 2", img: "img2.jpg"},
+    { auteur: "Antoine Daniel", type: "voix", son: 'popopo.mp3', titre: "Photo 3", img: "img3.jpg"},
+    { auteur: "Antoine Daniel", type: "voix", son: 'eddy.mp3', titre: "Photo 4", img: "img4.jpg"},
+    { auteur: "Antoine Daniel", type: "voix", son: 'russie.mp3', titre: "Photo 5", img: "img5.jpg"},
+    { auteur: "Antoine Daniel", type: "voix", son: 'psycho.mp3', titre: "Photo 6", img: "img6.jpg"},
+    { auteur: "Antoine Daniel", type: "voix", son: 'salutAD.mp3', titre: "Photo 7", img: "img7.jpg"},
+    { auteur: "South Park", type: "animé", son: 'gogole.mp3', titre: "Photo 8", img: "img8.jpg"},
+    { auteur: "leeroy Jenkins", type: "jeux", son: 'leeroy.mp3', titre: "Photo 9", img: "img9.jpg"},
+    { auteur: "Wilhelm", type: "voix", son: 'Wilhelm.mp3', titre: "Photo 10", img: "img10.jpg"},
+    { auteur: "Naheulbeuk", type: "voix", son: 'nain.mp3', titre: "Photo 11", img: "img11.jpg"},
+    { auteur: "Game Of Thrones", type: "voix", son: 'hodor.mp3', titre: "Photo 12", img: "img12.jpg"},
+    { auteur: "Kaamlott", type: "voix", son: 'perceval.mp3', titre: "Photo 13", img: "img13.jpg"},
+    { auteur: "Kaamlott", type: "voix", son: 'gras.mp3', titre: "Photo 14", img: "img14.jpg"}
+] 
 
